38a991dbdced364b9fdd92b2211ac81dc3aa6f5f
Wei-Hsiang86 <frankflyhuang@gmail.com>
2024-09-09 20:30:32 +0800

    add admin index page

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
+const admin = require('./modules/admin')
+
 const userController = require('../controllers/user-controller')
+
+router.use('/admin', admin)
 
 router.get('/users', userController.getUsers)
 
-router.get('/', (req, res) => { res.redirect('/users') })
+router.use('/', (req, res) => { res.redirect('/users') })
 
 module.exports = router
