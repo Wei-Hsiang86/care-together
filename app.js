ba948842009e08c4557387a8301da819f98fcb55
Wei-Hsiang86 <frankflyhuang@gmail.com>
2024-09-09 17:35:11 +0800

    add index page

--- app.js
+++ app.js
@@ -11,5 +11,3 @@ app.use(routes)
 app.listen(port, () => {
   console.info(`Example app listening on port ${port}!`)
 })
-
-module.exports = app
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.send('Hello World!!')
-})
+const userController = require('../controllers/user-controller')
+
+router.get('/users', userController.getUsers)
+
+router.get('/', (req, res) => { res.redirect('/users') })
 
 module.exports = router
