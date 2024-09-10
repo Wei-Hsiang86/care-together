9e3cb98d657a45b70ffaeda3cc69bcdfc5064730
Wei-Hsiang86 <frankflyhuang@gmail.com>
2024-09-11 02:50:11 +0800

    add sign in & init passport

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express')
 const handlebars = require('express-handlebars')
 const flash = require('connect-flash')
 const session = require('express-session')
+const passport = require('./config/passport')
 const routes = require('./routes')
+
 const app = express()
 const port = process.env.PORT || 3000
 const SESSION_SECRET = 'secret'
@@ -13,6 +15,8 @@ app.use(express.urlencoded({ extended: true }))
 
 // 驗證
 app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(flash())
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
@@ -23,5 +27,5 @@ app.use((req, res, next) => {
 app.use(routes)
 
 app.listen(port, () => {
-  console.info(`Example app listening on port ${port}!`)
+  console.info(`Care together is listening on port ${port}!`)
 })
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
+const passport = require('../config/passport')
+
 const admin = require('./modules/admin')
 
 const vsignController = require('../controllers/vsign-controller')
@@ -11,6 +13,10 @@ router.use('/admin', admin)
 
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
+
+router.get('/signin', userController.signInPage)
+router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
+router.get('/logout', userController.logout)
 
 router.get('/vsigns', vsignController.getVsigns)
 
